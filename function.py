704857dc7be21dc8b2b80a4f5e1e996299112d91
Jermmy <xuyongzhe94@gmail.com>
2023-06-18 18:24:25 +0800

    add QSigmoid

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 from torch.autograd import Function
+import torch
 
 
 class FakeQuantize(Function):
@@ -12,3 +13,21 @@ class FakeQuantize(Function):
     @staticmethod
     def backward(ctx, grad_output):
         return grad_output, None
+    
+def interp(x: torch.Tensor, xp: torch.Tensor, fp: torch.Tensor) -> torch.Tensor:
+    x_ = x.reshape(x.size(0), -1)
+    xp = xp.unsqueeze(0)
+    fp = fp.unsqueeze(0)
+        
+    m = (fp[:,1:] - fp[:,:-1]) / (xp[:,1:] - xp[:,:-1])  #slope
+    b = fp[:, :-1] - (m.mul(xp[:, :-1]) )
+
+    indicies = torch.sum(torch.ge(x_[:, :, None], xp[:, None, :]), -1) - 1  #torch.ge:  x[i] >= xp[i] ? true: false
+    indicies = torch.clamp(indicies, 0, m.shape[-1] - 1)
+
+    line_idx = torch.linspace(0, indicies.shape[0], 1, device=indicies.device).to(torch.long)
+    line_idx = line_idx.expand(indicies.shape)
+    # idx = torch.cat([line_idx, indicies] , 0)
+    out = m[line_idx, indicies].mul(x_) + b[line_idx, indicies]
+    out = out.reshape(x.shape)
+    return out
--- module.py
+++ module.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from function import FakeQuantize
+from function import FakeQuantize, interp
 
 
 def calcScaleZeroPoint(min_val, max_val, num_bits=8):
@@ -412,3 +412,53 @@ class QConvBNReLU(QModule):
         x.clamp_(0., 2.**self.num_bits-1.).round_()
         return x
         
+
+class QSigmoid(QModule):
+
+    def __init__(self, qi=True, qo=True, num_bits=8, lut_size=64):
+        super(QSigmoid, self).__init__(qi=qi, qo=qo, num_bits=num_bits)
+        self.num_bits = num_bits
+        self.lut_size = lut_size
+    
+    def forward(self, x):
+        if hasattr(self, 'qi'):
+            self.qi.update(x)
+            x = FakeQuantize.apply(x, self.qi)
+
+        x = torch.sigmoid(x)
+
+        if hasattr(self, 'qo'):
+            self.qo.update(x)
+            x = FakeQuantize.apply(x, self.qo)
+
+        return x
+    
+    def freeze(self, qi=None, qo=None):
+        if hasattr(self, 'qi') and qi is not None:
+            raise ValueError('qi has been provided in init function.')
+        if not hasattr(self, 'qi') and qi is None:
+            raise ValueError('qi is not existed, should be provided.')
+
+        if hasattr(self, 'qo') and qo is not None:
+            raise ValueError('qo has been provided in init function.')
+        if not hasattr(self, 'qo') and qo is None:
+            raise ValueError('qo is not existed, should be provided.')
+
+        if qi is not None:
+            self.qi = qi
+        if qo is not None:
+            self.qo = qo
+
+        lut_qx = torch.tensor(np.linspace(0, 2 ** self.num_bits - 1, self.lut_size), dtype=torch.uint8)
+        lut_x = self.qi.dequantize_tensor(lut_qx)
+        lut_y = torch.sigmoid(lut_x)
+        lut_qy = self.qo.quantize_tensor(lut_y)
+
+        self.register_buffer('lut_qy', lut_qy)
+        self.register_buffer('lut_qx', lut_qx)
+
+
+    def quantize_inference(self, x):
+        y = interp(x, self.lut_qx, self.lut_qy)
+        y = y.round_().clamp_(0., 2.**self.num_bits-1.)
+        return y
